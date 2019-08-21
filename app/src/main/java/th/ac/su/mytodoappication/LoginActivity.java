57f66f1900ec798a5ddfc6122128d3286ba541e2
Sorathan <53649479+Sorthan@users.noreply.github.com>
2019-08-21 21:02:29 +0700

    Update LoginActivity.java
    Add Toast 
    To have any Feature

--- app/src/main/java/th/ac/su/mytodoappication/LoginActivity.java
+++ app/src/main/java/th/ac/su/mytodoappication/LoginActivity.java
@@ -11,6 +11,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Toast t1 = Toast.makeText(LoginActivity.this,"WELCOME TO",Toast.LENGTH_LONG);
+        t1.show();
+        Toast t2 = Toast.makeText(LoginActivity.this,"My ToDo APPLICATION",Toast.LENGTH_SHORT);
+        t2.show();
     }
 }
